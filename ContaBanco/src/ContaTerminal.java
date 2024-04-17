439d2e80340b11f71bdad051f0dbe8d59ca1c019
KAlexandrino <kaio2808@gmail.com>
2024-04-17 20:57:20 -0300

    projeto finalizado e com comentarios

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,21 +1,22 @@
-        //TODO: Conhecer e importar a classe Scanner
-        // Exibir as mensagens para o usuario
-        // Obter pela Scanner os valores digitados no terminal
-        // Exibir a mensagem de conta criada 
+// TODO: Conhecer e importar a classe Scanner - OK
+// Exibir as mensagens para o usuario - OK
+// Obter pela Scanner os valores digitados no terminal - OK
+// Exibir a mensagem de conta criada  - OK
 
-import java.util.Scanner;
-import java.util.Locale;
+// importando os recursos que vou utilizar
+import java.util.Scanner; // Scanner para ler e armazenar os dados do usuario
+import java.util.Locale; // Locale para regionalizar o metodo de dados que vão ser inseridos via usuario
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US); // Iniciando um novo Scanner e regionalizando o Locale para US
     
         System.out.println("Por favor, digite o número da Conta!");
         int conta = scanner.nextInt();
 
         System.out.println("Por favor, digite o número da Agencia!");
-        scanner.nextLine();
+        scanner.nextLine(); // Utilizando um nextLine para correção de um erro que estava sendo apresentado, descartando uma entrada de dados invalida.
         String agencia = scanner.nextLine();
 
         System.out.println("Por favor, digite o nome completo do titular da conta!");
@@ -24,10 +25,10 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o saldo atual da conta!");
         double saldo = scanner.nextDouble();
 
-        scanner.close();
+        scanner.close(); //Fechando o Scanner boa boa convenção para não continuar utilizando recursos.
 
-        System.out.printf(
-    "Olá %s, obrigado por criar uma conta em nosso banco, sua agencia é %s, conta %d e seu saldo %.2f já está disponível para saque.", 
+        System.out.printf( // Concatenando utilizando o metodo printf.
+    "Olá %s, obrigado por criar uma conta em nosso banco, sua agencia é %s, conta %d e seu saldo %.2f já está disponível para saque.",  
                 nomeTitular, 
                 agencia,
                 conta,
